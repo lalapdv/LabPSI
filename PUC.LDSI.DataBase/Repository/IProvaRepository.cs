8fe30909e5e5473e30d3a528242e6a66c56cceb9
Carol <carolina.romanzini@sga.pucminas.br>
2020-06-06 20:03:43 -0300

    Repositoriosv1

---
+++ PUC.LDSI.DataBase/Repository/IProvaRepository.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PUC.LDSI.Domain.Entities;
+using PUC.LDSI.Domain.Interfaces.Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PUC.LDSI.DataBase.Repository
+{
+    public class ProvaRepository : Repository<Prova>
+    {
+        private readonly AppDbContext _context;
+
+        public ProvaRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Prova>> ObterPorAvaliacaoAlunoAsync(int idAvaliacao, int idAluno)
+        {
+            var query = _context.Prova
+                .Where(x => x.AvaliacaoId == idAvaliacao).Where(x => x.AlunoId == idAluno);
+
+            return await query.ToListAsync();
+        }
+        public override async Task<Prova> ObterAsync(int id)
+        {
+            var prova = await _context.Prova
+                .Include(x => x.QuestoesProva)
+                .ThenInclude(y => y.QuestaoAvaliacao)
+                .ThenInclude(z => z.Opcoes)
+                .ThenInclude(t => t.OpcoesProva)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return prova;
+        }
+
+    }
+}
